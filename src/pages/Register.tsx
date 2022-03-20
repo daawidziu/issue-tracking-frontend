f5bdcba56d8e8e27b963127cc66031b04e4e5e72
daawidziu <daawidziu@int.pl>
2022-03-20 20:12:25 +0100

    Create Register page & form

---
+++ src/pages/Register.tsx
@@ -0,0 +1,55 @@
+import {SubmitHandler, useForm} from "react-hook-form";
+import styles from '../styles/pages/_Register.module.sass';
+import stylesBtn from '../styles/components/_button.module.sass';
+import infoIcon from '../images/info.png';
+import Api from "../services/api";
+import {useAuth} from "../contexts/AuthContext";
+import {useState} from "react";
+
+type Inputs = {
+    email: string,
+    password: string
+}
+
+const Register = () => {
+    const {register, handleSubmit, formState: {errors, isValid, isSubmitted}} = useForm<Inputs>();
+    const [message, setMessage] = useState<string>('')
+    const onSubmit: SubmitHandler<Inputs> = data => Api.post('auth/register', data)
+        .then(response => {
+            auth.dispatch({type: 'login'})
+        })
+        .catch(error => setMessage('Email already registered!'))
+
+    const auth = useAuth();
+
+    return (
+        <div className={styles.registerContainer}>
+            <h1 className={styles.registerTitle}>Register</h1>
+            <form onSubmit={handleSubmit(onSubmit)} className={styles.registerForm}>
+                <input type='email' placeholder={'email'} {...register('email',
+                    {required: true, pattern: /[^@]+@[^@]+\.[^@]+/})} className={styles.registerInput}/>
+                <input type='password' placeholder={'password'} {...register('password', {
+                    required: true,
+                    pattern: /^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$/
+                })} className={styles.registerInput}/>
+                <span className={styles.registerInfo}>
+                    <img src={infoIcon} alt={'Information icon'}/>
+                    {' '} Your password must contain at least 8 characters, one symbol and number
+                </span>
+                <span className={styles.registerInfoError}>
+                    {(!isValid && isSubmitted) ? 'Wrong password' : <br/>}
+                </span>
+                <span className={styles.registerInfoError}>
+                    {message}
+                </span>
+                <input type='submit' value={'Register'}
+                       className={stylesBtn.btn + ' ' + stylesBtn.btnSmall + ' ' + stylesBtn.btnSecondary}/>
+            </form>
+            <button className={stylesBtn.btn + ' ' + stylesBtn.btnSmall + ' ' + stylesBtn.btnOff}>
+                Login as Guest
+            </button>
+        </div>
+    );
+}
+
+export default Register;
