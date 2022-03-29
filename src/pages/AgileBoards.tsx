628fec19a541505b94745c8fbce445b27c8c368d
daawidziu <daawidziu@int.pl>
2022-03-29 12:41:27 +0200

    Create Agile Boards page

--- src/pages/AgileBoards.tsx
+++ src/pages/AgileBoards.tsx
@@ -1,9 +1,41 @@
+import {useState, useEffect} from "react";
+import Api from "../services/api";
+import Loading from '../images/Pulse-1.3s-200px.svg';
+import {Issue} from "../types";
+import {useAuth} from "../contexts/AuthContext";
+import stylesTop from "../styles/components/_TopContainer.module.sass";
+import stylesDD from '../styles/components/_DragDrop.module.sass';
+import IssueDragList from "../components/IssueDragList";
+
+
 const AgileBoards = () => {
-    return (
-        <div>
-        <h1>Work in Progress</h1>
-        <p>Available soon</p>
-        </div>
+    const [data, setData] = useState<Issue[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    const auth = useAuth();
+
+    useEffect(() => {
+            Api.get('issues/')
+                .then(r => setData(r.data))
+                .catch(r => setIsLoading(true))
+                .finally(() => setIsLoading(false));
+    }, []);
+
+    return (<>
+                            {!auth.state.logged &&
+                        <div className={stylesTop.topContainer}>
+                            <span>Log in to change issue status</span>
+                        </div>}
+        <section className={stylesDD.container}>
+            {isLoading ?
+                <img src={Loading} alt={'Loading Animation'}/> :
+                <>
+
+
+                        <IssueDragList issues={data} setIssues={setData}/>
+                </>
+            }
+        </section></>
     );
 }
 
